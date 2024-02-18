b5baa369c671313fe49ddbc826f8602e45166ff3
Chris Angelo Averia <averiac@vmware.com>
2024-02-18 11:57:04 +0800

    commit

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,11 +21,11 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("Greetings from Spring Boot 23!", controller.index());
+        assertEquals("Greetings from Spring Boot 24!", controller.index());
 
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from Spring Boot 23!"));
+            .andExpect(content().string("Greetings from Spring Boot 24!"));
     }
 }
